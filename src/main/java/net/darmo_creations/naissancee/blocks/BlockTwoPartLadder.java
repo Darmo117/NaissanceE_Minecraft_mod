83169918a92f936c888adb75ff00078864baa532
Darmo117 <darmo117@gmail.com>
2021-11-11 21:28:26 +0100

    two-part ladder no longer disappears when support block is removed

--- src/main/java/net/darmo_creations/naissancee/blocks/BlockTwoPartLadder.java
+++ src/main/java/net/darmo_creations/naissancee/blocks/BlockTwoPartLadder.java
@@ -1,9 +1,27 @@
 package net.darmo_creations.naissancee.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockLadder;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 /**
- * Dummy class to make {@link BlockLadder}’s constructor public.
+ * Custom ladder that does not disappear if the block it is placed against is removed.
  */
 public class BlockTwoPartLadder extends BlockLadder implements IModBlock {
+  @Override
+  public void neighborChanged(IBlockState state, World world, BlockPos pos, Block block, BlockPos fromPos) {
+    EnumFacing enumfacing = state.getValue(FACING);
+
+    if (!this.isPistonPart(world, pos.offset(enumfacing.getOpposite()))) {
+      this.dropBlockAsItem(world, pos, state, 0);
+      world.setBlockToAir(pos);
+    }
+  }
+
+  private boolean isPistonPart(World world, BlockPos pos) {
+    return !isExceptBlockForAttachWithPiston(world.getBlockState(pos).getBlock());
+  }
 }
