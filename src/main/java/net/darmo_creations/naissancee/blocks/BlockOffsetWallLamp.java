a53b2dabf8eb530707716be00dc909cfe398a480
Darmo117 <darmo117@gmail.com>
2021-11-11 21:57:37 +0100

    offset lamp can now be placed horizontally

--- src/main/java/net/darmo_creations/naissancee/blocks/BlockOffsetWallLamp.java
+++ src/main/java/net/darmo_creations/naissancee/blocks/BlockOffsetWallLamp.java
@@ -1,6 +1,6 @@
 package net.darmo_creations.naissancee.blocks;
 
-import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
@@ -18,11 +18,13 @@ import net.minecraft.world.World;
 /**
  * A small thin vertical luminous block that is offset by half a block.
  */
-public class BlockOffsetWallLamp extends BlockHorizontal implements IModBlock {
-  private static final AxisAlignedBB AABB_NORTH = new AxisAlignedBB(0.6875, 0.1875, 0.875, 1.3125, 0.8125, 1.0);
-  private static final AxisAlignedBB AABB_SOUTH = new AxisAlignedBB(-0.3125, 0.1875, 0.0, 0.3125, 0.8125, 0.125);
-  private static final AxisAlignedBB AABB_WEST = new AxisAlignedBB(0.875, 0.1875, -0.3125, 1.0, 0.8125, 0.3125);
-  private static final AxisAlignedBB AABB_EAST = new AxisAlignedBB(0.0, 0.1875, 0.6875, 0.125, 0.8125, 1.3125);
+public class BlockOffsetWallLamp extends BlockDirectional implements IModBlock {
+  private static final AxisAlignedBB AABB_NORTH = new AxisAlignedBB(0.6875, 0.1875, 0.875, 1.3125, 0.8125, 1);
+  private static final AxisAlignedBB AABB_SOUTH = new AxisAlignedBB(-0.3125, 0.1875, 0, 0.3125, 0.8125, 0.125);
+  private static final AxisAlignedBB AABB_WEST = new AxisAlignedBB(0.875, 0.1875, -0.3125, 1, 0.8125, 0.3125);
+  private static final AxisAlignedBB AABB_EAST = new AxisAlignedBB(0, 0.1875, 0.6875, 0.125, 0.8125, 1.3125);
+  private static final AxisAlignedBB AABB_UP = new AxisAlignedBB(0.6875, 0, 0.1875, 1.3125, 0.125, 0.8125);
+  private static final AxisAlignedBB AABB_DOWN = new AxisAlignedBB(0.6875, 0.875, 0.1875, 1.3125, 1, 0.8125);
 
   public BlockOffsetWallLamp() {
     super(Material.GLASS);
@@ -47,8 +49,12 @@ public class BlockOffsetWallLamp extends BlockHorizontal implements IModBlock {
         return AABB_WEST;
       case EAST:
         return AABB_EAST;
+      case DOWN:
+        return AABB_DOWN;
+      case UP:
+      default:
+        return AABB_UP;
     }
-    return null; // Should never happen
   }
 
   @Override
@@ -90,18 +96,18 @@ public class BlockOffsetWallLamp extends BlockHorizontal implements IModBlock {
   @Override
   public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
     return super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer)
-        .withProperty(FACING, (facing == EnumFacing.UP || facing == EnumFacing.DOWN) ? placer.getHorizontalFacing().getOpposite() : facing);
+        .withProperty(FACING, facing);
   }
 
   @SuppressWarnings("deprecation")
   @Override
   public IBlockState getStateFromMeta(int meta) {
-    return this.getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
+    return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta));
   }
 
   @Override
   public int getMetaFromState(IBlockState state) {
-    return state.getValue(FACING).getHorizontalIndex();
+    return state.getValue(FACING).getIndex();
   }
 
   @SuppressWarnings("deprecation")
